f3c973d3d831c787b00399611b68165da7044031
bewburats <bew25442@gmail.com>
2022-02-17 19:30:07 +0700

    [B] draft

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,151 @@
 <script setup>
+import { ref } from 'vue';
+
+let colors = ['red', 'green', 'blue', 'pink', 'yellow', 'black', 'brown', 'orange', 'purple'];
+const aleartCorrect = ref('');
+const aleartInCorrect = ref('');
+const colorWord = ref('color 🇺🇸');
+const colorWordHead = ref('');
+const activeColor = ref('');
+const answer = ref('');
+const countTime = ref('');
+const countPoint = ref(0);
+// const inputColor = ref(answer.value);
+const todayColor = ref(colors[getTodayColorIndex()]);
+
+function getTodayColorIndex() {
+  let days = new Date().getDate();
+  let index = days % colors.length;
+  console.log(index);
+  return index;
+}
+
+function randColorHeadName() {
+  let colorHead = ['brown', 'pink', 'coral', 'green', 'red']
+  let randColorHead = colorHead[Math.floor(Math.random() * colorHead.length)];
+  colorWordHead.value = randColorHead;
+}
+
+function randWebName() {
+  let nationColor = ['颜 色 🇨🇳', 'colour 🇬🇧', 'color 🇺🇸', 'Farbe 🇩🇪 ', 'Couleur 🇫🇷', 'renk 🇹🇷', 'kleur 🇳🇱'];
+  let randColorName = nationColor[Math.floor(Math.random() * nationColor.length)];
+  colorWord.value = randColorName;
+}
+
+setInterval(randColorHeadName, 1000)
+setInterval(randWebName, 2000);
+
+// function checkInputColor(){
+//   inputColor.value =`${answer.value}`
+// }
+
+function checkGuessWord() {
+  setInterval(setTime, 1000);
+  if (answer.value === todayColor.value) {
+    setTime()
+    activeColor.value = answer.value
+    console.log(todayColor.value);
+    console.log(activeColor.value);
+    console.log(answer.value);
+    aleartInCorrect.value = ''
+    aleartCorrect.value = 'Your correct , Cool !';
+    console.log("Correct");
+  } else {
+    setTime()
+    console.log("false");
+    console.log(todayColor.value);
+    console.log(activeColor.value);
+    console.log(answer.value);
+    aleartCorrect.value = ''
+    aleartInCorrect.value = 'Incorrect ! please try again';
+    activeColor.value = 'white'
+  }
+}
+
+function setTime() {
+  let crrDate = new Date();
+  let tmrDate = new Date();
+  tmrDate.setHours(24, 0, 0, 0);
+  let diffMS = tmrDate.getTime() / 1000 - crrDate.getTime() / 1000;
+  let diffHr = Math.floor(diffMS / 3600);
+  diffMS = diffMS - diffHr * 3600;
+  let diffMi = Math.floor(diffMS / 60);
+  diffMS = diffMS - diffMi * 60;
+  let diffS = Math.floor(diffMS);
+  let result = ((diffHr < 10) ? "0" + diffHr : diffHr);
+  result += ":" + ((diffMi < 10) ? "0" + diffMi : diffMi);
+  result += ":" + ((diffS < 10) ? "0" + diffS : diffS);
+  countTime.value = `${result}`  
+}
+
 
 </script>
  
 <template>
-  <h1 class="text-3xl font-bold underline">
-    C O L O R !
-  </h1>
+  <div
+    id="back"
+    class="min-h-screen bg-gray-50 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12"
+    :style="{ 'background-color': activeColor }"
+  >
+    <div
+      class="dark:bg-slate-800 dark:text-white relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10"
+    >
+      <div class="max-w-md mx-auto">
+        <div class="h-6 font-medium text-xl" :style="{ 'color': colorWordHead }">🎨 C O L O R S</div>
+
+        <br />
+        <hr />
+        <div class="divide-y divide-gray-300/50">
+          <div class="dark:text-white py-8 text-base leading-7 space-y-6 text-gray-600">
+            <p>Guess {{ colorWord }} !</p>
+            <div class="grid grid-flow-col auto-cols-max">
+              <div class="relative z-0 mb-6 w-full group">
+                <input
+                  @keyup.enter="checkGuessWord"
+                  v-model="answer"
+                  type="text"
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder="Input Colors" :style="{ 'color': answer }"
+                  required
+                />
+              </div>
+              <div>
+                <button
+                  class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-cyan-200 dark:focus:ring-cyan-800"
+                >
+                  <span
+                    @click="checkGuessWord"
+                    class="py-2 relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-slate-800 rounded-md group-hover:bg-opacity-0"
+                  >Check it !</span>
+                </button>
+              </div>
+            </div>
+
+            <span
+              v-show="aleartInCorrect"
+              class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900"
+            >{{ aleartInCorrect }}</span>
+            <span
+              v-show="aleartCorrect"
+              class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900"
+            >{{ aleartCorrect }}</span>
+            <div v-show="countTime">
+              You can guess it every day,see you in
+              <span
+                class="font-medium italic bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-900"
+              >{{ countTime }}</span>
+              
+            </div>
+            <div>
+              Total Score:
+              <span class="font-medium bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-900">{{ countPoint }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
  
 <style>
-
 </style>
