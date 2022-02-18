7b7f919772aa3f3b50e58691e2dc5306593ad91b
bewburats <bew25442@gmail.com>
2022-02-18 20:46:29 +0700

    [G]add info

--- src/App.vue
+++ src/App.vue
@@ -93,7 +93,7 @@ function hint(){
     countPoint.value = 80
   }
 }
-
+const showModal = ref(false); //popup
 </script>
  
 <template>
@@ -113,6 +113,81 @@ function hint(){
     <div
       class="dark:bg-slate-800 dark:text-white relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10"
     >
+    <div div class="flex justify-end">
+        <button
+          
+          type="button"
+          data-modal-toggle="defaultModal"
+        >
+        
+          <svg 
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    <div
+        id="defaultModal"
+        aria-hidden="true"
+        class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center h-modal md:h-full md:inset-0"
+      >
+        <div class="relative px-4 w-full max-w-2xl h-full md:h-auto">
+          <!-- Modal content -->
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div
+              class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600"
+            >
+    <h3
+                class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white"
+              >
+                How To Play!
+              </h3>
+              <button
+                type="button"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-toggle="defaultModal"
+              >
+                <svg
+                  class="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+                <!-- Modal body -->
+            <div class="p-6 space-y-6">
+              <p
+                class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+              >
+                info
+              </p>
+              <p
+                class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+              >
+                info
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="max-w-md mx-auto">
         <div class="h-6 font-medium text-xl" :style="{ 'color': colorWordHead }">🎨 C O L O R S</div>
 
@@ -205,5 +280,28 @@ h3{
   text-align:center;
   font-size:1.6em;
   cursor:pointer;
+}
+
+#app {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: right;
+
+  width: auto;
+  min-height: auto;
+  overflow-x: hidden;
+}
+
+.button {
+  appearance: none;
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+
+  display: inline-block;
+  padding: 5px 5px;
+  background-image: linear-gradient(to right, #61daf8, #4290f7);
 }
 </style>
