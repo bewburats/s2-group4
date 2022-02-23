32a6fe980da57ac37935f34eb82c0af699acbce1
bewburats <bew25442@gmail.com>
2022-02-23 12:32:20 +0700

    [B]change dark mode

--- src/App.vue
+++ src/App.vue
@@ -214,7 +214,7 @@ setInterval(setTime, 1000);
             </div>
 
             <div
-              class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+              class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
               role="alert"
               id="correct"
               v-show="isShowAnswer"
